0bf1138351e5af5b6db2c6a83c7d25699225ceb1
ahnafb <ahnafb@students.unnes.ac.id.com>
2024-02-12 09:56:59 +0700

    redesign and add sidebar

--- app.py
+++ app.py
@@ -23,5 +23,9 @@ def home():
 def home2():
     return render_template("index2.html")
 
+@app.route("/list", methods=['GET'])
+def list():
+    return render_template("list.html")
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
